521790eb038f5c2efeb0cbee0ad4ea54b3c7610b
Ramin Rahimzada <raminrahimzada@gmail.com>
2021-06-14 16:36:10 +0400

    Update and rename ExampleQuery.cs to Demo/ExampleQuery.cs

--- Demo/ExampleQuery.cs
+++ Demo/ExampleQuery.cs
@@ -5,13 +5,13 @@ namespace raminrahimzada
 {
     public class ExampleQuery : AbstractQuery<string>
     {
-        //TODO
+        //TODO your query parameters here
     }
     public partial class Handler:IQueryHandler<ExampleQuery,string>
     {
         public async Task<ExecutionResult<string>> Handle(ExampleQuery query, CancellationToken cancellationToken)
         {
-            //TODO
+            //TODO your query handling jobs here
             return ExecutionResult.Success("hello World");
         }
     }
